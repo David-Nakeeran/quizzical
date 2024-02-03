f663d9a7b914335a5fb32a03408212df5baff077
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2024-02-03 09:43:45 +0000

    Added state isQuizAlive. Ternary to display quiz or start quiz screens based on state.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import { useState, useEffect } from 'react';
+import Start from './Start';
+import Quiz from './Quiz';
 'use strict';
 
 function App() {
   
+
+  function startQuiz() {
+    setIsQuizAlive(prevState => !prevState)
+  }
+  
+  const [isQuizAlive, setIsQuizAlive] = useState(false);
+
   return (
-    <>
-    </>
+    <main className='container'>
+      {isQuizAlive ? <Quiz /> : <Start startQuiz={() => startQuiz()}/>}
+      
+    </main>
   )
 }
 
